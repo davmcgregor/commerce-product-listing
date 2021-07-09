3c8c27c2bfe40b61ff39957a95f420d1e44f2465
davidmcgregor <davidjohnmcgregor@gmail.com>
2021-07-09 11:28:49 +0100

    Streamline util functions

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,16 +1,12 @@
 export const searchProducts = (currentProducts, term) => {
-  return currentProducts
-    .filter(
-      (product) =>
-        `${product.productName} ${product.type}`
-          .toLowerCase()
-          .indexOf(term.toLowerCase()) >= 0
-    )
-    .map((product) => product);
+  return currentProducts.filter(
+    (product) =>
+      `${product.productName} ${product.type}`
+        .toLowerCase()
+        .indexOf(term.toLowerCase()) >= 0
+  );
 };
 
 export const selectProducts = (currentProducts, value) => {
-  return currentProducts
-    .filter((product) => product.type === value)
-    .map((product) => product);
+  return currentProducts.filter((product) => product.type === value);
 };
